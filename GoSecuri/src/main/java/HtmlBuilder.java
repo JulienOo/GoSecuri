215a751141f511aa89399ec818ded83295bf563e
nathanlanglet <nathan-51@hotmail.fr>
2022-05-06 09:08:55 +0200

    Remove getHTML

--- GoSecuri/src/main/java/HtmlBuilder.java
+++ GoSecuri/src/main/java/HtmlBuilder.java
@@ -45,17 +45,4 @@ public class HtmlBuilder {
         }
     }
 
-    public static String getHTML(String urlToRead) throws IOException {
-        StringBuilder result = new StringBuilder();
-        URL url = new URL(urlToRead);
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        conn.setRequestMethod("GET");
-        try (BufferedReader reader = new BufferedReader(
-                new InputStreamReader(conn.getInputStream()))) {
-            for (String line; (line = reader.readLine()) != null; ) {
-                result.append(line);
-            }
-        }
-        return result.toString();
-    }
 }
